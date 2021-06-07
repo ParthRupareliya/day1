3b43531b15bb890dbbdec51df67522dbb6c9b478
ParthRupareliya <85487931+ParthRupareliya@users.noreply.github.com>
2021-06-07 20:17:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>home page</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <link rel="stylesheet" href="http://www.bechobooks.in/assets/css/custom-fonticons.css">
+</head>
+<body>
+  
+  <div class="card text-center">
+  <div class="card-header" style="margin-top: 5px;">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" href="C:\xampp\htdocs\project\index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="product.php">Products</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="faqs.php">Faqs</a>
+      </li>
+      
+      <li class="nav-item">
+          <a href="login.php" class="nav-link" style="margin-left: 600px;" >Login</a>
+      </li>
+      <li class="nav-item">
+          <a href="register.php" class="nav-link" >Register</a>
+         
+      </li>
+
+    
+    </ul>
+  </div>
+  <div class="card-body" style="margin-top: 2px;">
+ 
+    <img src="img\logo.jpg" class="img-thumbnail" alt="Responsive image" height="100px" width="150px" align="left">
+
+    
+  </div>
+</div>
+
+<div class="container text-center">
+        <header>
+          <h2><span class="text-primary">Our Services</span> <br>
+            What do <span style="color: red;">PJ</span> provide</h2>
+          <p class="lead col-md-10 mx-auto">this is a platform for buying the best grocery & worship material online. here you can easily get products at your doorstep. With the use of our services your shopping become more easy.
+ </p></br></br>
+
+ <div class="row">
+          <!-- Item-->
+          <div class="item col-lg-4">
+            <div class="icon"><img src="img/mission.png"></div>
+            <div class="text">
+              <h3 class="text-primary">OUR MISSION</h3>
+              <p class="text-muted">after order given delivery before time, customer satisfaction, & be the best store in the town  is our main mission.
+</p>
+            </div>
+          </div>
+          <!-- Item-->
+          <div class="item col-lg-4">
+            <div class="icon"><img src="img/vision.png"></div>
+            <div class="text">
+              <h3 class="text-primary">OUR VISION</h3>
+              <p class="text-muted">our main vision is that with the use of our website customer can easily purchase any time from their home.</p>
+            </div>
+          </div>
+          <!-- Item-->
+          <div class="item col-lg-4">
+            <div class="icon"><img src="img/custo.png"></div>
+            <div class="text">
+              <h3 class="text-primary">Customer Support</h3>
+              <p class="text-muted">we are always available for our customer. they can asks/request us anything about product and our services. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+
+
+
+
+
+<!-- footer start-->
+  <footer class="p-3 mb-2 bg-dark text-white" style="bottom: 0; margin-top: 4px;">
+      <div class="container">
+        <div class="row">
+          <div class="about col-md-4">
+            <div class="logo"><img src="img\logo.jpg" alt="logo" height="60"></div>
+            <p>Bechobooks provide
+this is a platform for buying the best grocery & worship material online. here you can easily get products at your doorstep. With the use of our services your shopping become more easy.
+		</div>			
+          <div class="site-links col-md-4">
+            <h3>Useful links</h3>
+            <div class="info">
+              <ul class="list-unstyled" > 
+                <li> <a href="index.php">Homepage</a></li>
+                <li> <a href="books.php">Products </a></li>
+                <li> <a href="contact.php">Contact Us</a></li>
+            
+               <li> <a href="faqs.php">Faqs</a></li>
+                
+              </ul>
+             
+            </div>
+          </div>
+          <div class="contact col-md-4">
+            <h3>Contact  us</h3>
+            <div class="info">
+              <p>Junagadh(362001), gujrat.<br>
+              <span class="icon"><img src="img/whatsapp.png">  phone: 9664780136</span>
+
+              <br>
+              <span class="icon"><img src="img/email.png">Email: parthhrupareliya@gmail.com</span>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        <p>&copy;2021<span class="text-primary">PJ.</span> All Rights Reserved.</p>
+      </div>
+    </footer>
+
+</body>
+</html>
